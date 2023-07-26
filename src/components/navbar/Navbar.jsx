9c0cde3aa16c03261c18e83f3f3a24b0bee79c75
Q4119-Midnight <tanrivermis.mehmet@gmail.com>
2023-07-26 23:00:14 +0300

    updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { menuIcon } from "../../helper/iconData";
+import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  return (
+    <Nav justify="space-between" wrap="wrap">
+      <Logo>Clarus Library</Logo>
+      <HamburgerIcon onClick={() => setToggle(!toggle)}>
+        {menuIcon}
+      </HamburgerIcon>
+      <Menu showMenu={toggle}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/login">Login</MenuLink>
+        <MenuLink to="/register">Register</MenuLink>
+        <MenuLink to="/login">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+
+export default Navbar;
